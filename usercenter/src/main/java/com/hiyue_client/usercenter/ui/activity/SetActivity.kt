a2de39822cfa7b8cd6349d682279d6baed59e5c1
liqiangzsl <1094655541@qq.com>
2019-11-18 21:18:32 +0800

    设置界面变动

--- usercenter/src/main/java/com/hiyue_client/usercenter/ui/activity/SetActivity.kt
+++ usercenter/src/main/java/com/hiyue_client/usercenter/ui/activity/SetActivity.kt
@@ -2,19 +2,29 @@ package com.hiyue_client.usercenter.ui.activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import com.hiyue_client.usercenter.R
+import com.lib.network.ext.onClick
 import com.lib.network.ui.BaseActivity
 
 /**
  * 设置界面
  */
 class SetActivity : BaseActivity(){
+
+    private var but: Button? = null
+
     override fun initLayoutView(): Int {
         return R.layout.activity_set
     }
 
     override fun initView() {
+        but = findViewById(R.id.but)
+        but?.text = "退出登录"
+        but?.onClick {
+            //确定
 
+        }
     }
 
     override fun initIntentData(intent: Intent) {
