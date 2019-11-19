919dd8ce3667529572f3a0935b9e355bb1cb462e
liqiangzsl <1094655541@qq.com>
2019-11-19 13:35:49 +0800

    设置界面变动

--- app/src/main/java/com/chorus/hiyue_client/app/App.kt
+++ app/src/main/java/com/chorus/hiyue_client/app/App.kt
@@ -1,5 +1,6 @@
 package com.chorus.hiyue_client.app
 
+import android.support.multidex.MultiDex
 import com.alibaba.android.arouter.launcher.ARouter
 import com.lib.network.app.BaseApplication
 
@@ -8,5 +9,6 @@ class App : BaseApplication() {
     override fun onCreate() {
         super.onCreate()
         ARouter.init(this)
+        MultiDex.install(this)
     }
 }
